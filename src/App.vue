a8913b0a07dd48bc69af88fe9ccaa87f6cab0397
stevy <stevyralambomanana@gmail.com>
2023-07-08 18:52:40 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,33 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <nav class="menum navbar navbar-dark navbar-expand-md justify-content-center">
+      <div class="container">
+        <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
+          <ul class="topBotomBordersOut navbar-nav mx-auto text-center">
+            <li class="nav-item active">
+              <RouterLink class="nav-link" to="/">Chat</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/profile">User</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/login">Login</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/register">Register</RouterLink>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
-
   <RouterView />
 </template>
 
 <style scoped>
+  @import './assets/menu.css' ;
+
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ChatView from '../views/ChatView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'chat',
+      component: ChatView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      // component: () => import('../views/ProfileView.vue')
     }
   ]
 })
